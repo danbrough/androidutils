5e98b18b5b648584f0378521f35a9cadffe9ccda
Dan Brough <dan@danbrough.org>
2019-10-14 12:14:06 +1300

    added new android_resource package

--- android-ui/src/main/java/danbroid/util/ui/CursorRecyclerViewAdapter.java
+++ android-ui/src/main/java/danbroid/util/ui/CursorRecyclerViewAdapter.java
@@ -22,99 +22,99 @@ import android.database.DataSetObserver;
 import android.support.v7.widget.RecyclerView;
 
 public abstract class CursorRecyclerViewAdapter<VH extends RecyclerView.ViewHolder> extends
-		RecyclerView.Adapter<VH> {
-	private final DataSetObserver dataSetObserver;
-	private Cursor cursor;
-	private boolean dataValid;
-	private int rowIdColumn;
+    RecyclerView.Adapter<VH> {
+  private final DataSetObserver dataSetObserver;
+  private Cursor cursor;
+  private boolean dataValid;
+  private int rowIdColumn;
 
-	public CursorRecyclerViewAdapter() {
-		this(null);
-	}
+  public CursorRecyclerViewAdapter() {
+    this(null);
+  }
 
-	public CursorRecyclerViewAdapter(Cursor cursor) {
-		setHasStableIds(true);
-		dataValid = cursor != null;
-		rowIdColumn = dataValid ? cursor.getColumnIndex("_id") : -1;
-		dataSetObserver = new NotifyingDataSetObserver();
-		if (cursor != null) {
-			cursor.registerDataSetObserver(dataSetObserver);
-		}
-	}
+  public CursorRecyclerViewAdapter(Cursor cursor) {
+    setHasStableIds(true);
+    dataValid = cursor != null;
+    rowIdColumn = dataValid ? cursor.getColumnIndex("_id") : -1;
+    dataSetObserver = new NotifyingDataSetObserver();
+    if (cursor != null) {
+      cursor.registerDataSetObserver(dataSetObserver);
+    }
+  }
 
-	public Cursor getCursor() {
-		return cursor;
-	}
+  public Cursor getCursor() {
+    return cursor;
+  }
 
-	@Override
-	public int getItemCount() {
-		if (dataValid && cursor != null) {
-			return cursor.getCount();
-		}
-		return 0;
-	}
+  @Override
+  public int getItemCount() {
+    if (dataValid && cursor != null) {
+      return cursor.getCount();
+    }
+    return 0;
+  }
 
-	@Override
-	public long getItemId(int position) {
-		if (dataValid && cursor != null && cursor.moveToPosition(position)) {
-			return cursor.getLong(rowIdColumn);
-		}
-		return 0;
-	}
+  @Override
+  public long getItemId(int position) {
+    if (dataValid && cursor != null && cursor.moveToPosition(position)) {
+      return cursor.getLong(rowIdColumn);
+    }
+    return 0;
+  }
 
-	public abstract void onBindViewHolder(VH viewHolder, Cursor cursor);
+  public abstract void onBindViewHolder(VH viewHolder, Cursor cursor);
 
-	@Override
-	public final void onBindViewHolder(VH viewHolder, int position) {
-		if (!dataValid) {
-			throw new IllegalStateException("this should only be called when the cursor is valid");
-		}
-		if (!cursor.moveToPosition(position)) {
-			throw new IllegalStateException("couldn't move cursor to position " + position);
-		}
-		onBindViewHolder(viewHolder, cursor);
-	}
+  @Override
+  public final void onBindViewHolder(VH viewHolder, int position) {
+    if (!dataValid) {
+      throw new IllegalStateException("this should only be called when the cursor is valid");
+    }
+    if (!cursor.moveToPosition(position)) {
+      throw new IllegalStateException("couldn't move cursor to position " + position);
+    }
+    onBindViewHolder(viewHolder, cursor);
+  }
 
 
-	public Cursor swapCursor(Cursor newCursor) {
+  public Cursor swapCursor(Cursor newCursor) {
 
-		if (newCursor == cursor) {
-			return null;
-		}
-		final Cursor oldCursor = cursor;
-		if (oldCursor != null && dataSetObserver != null) {
-			oldCursor.unregisterDataSetObserver(dataSetObserver);
-		}
-		cursor = newCursor;
-		if (cursor != null) {
-			if (dataSetObserver != null) {
-				cursor.registerDataSetObserver(dataSetObserver);
-			}
-			rowIdColumn = newCursor.getColumnIndexOrThrow("_id");
-			dataValid = true;
-			notifyDataSetChanged();
-		} else {
-			rowIdColumn = -1;
-			dataValid = false;
-			notifyDataSetChanged();
-		}
-		return oldCursor;
-	}
+    if (newCursor == cursor) {
+      return null;
+    }
+    final Cursor oldCursor = cursor;
+    if (oldCursor != null && dataSetObserver != null) {
+      oldCursor.unregisterDataSetObserver(dataSetObserver);
+    }
+    cursor = newCursor;
+    if (cursor != null) {
+      if (dataSetObserver != null) {
+        cursor.registerDataSetObserver(dataSetObserver);
+      }
+      rowIdColumn = newCursor.getColumnIndexOrThrow("_id");
+      dataValid = true;
+      notifyDataSetChanged();
+    } else {
+      rowIdColumn = -1;
+      dataValid = false;
+      notifyDataSetChanged();
+    }
+    return oldCursor;
+  }
 
-	private class NotifyingDataSetObserver extends DataSetObserver {
-		@Override
-		public void onChanged() {
-			super.onChanged();
-			dataValid = true;
-			notifyDataSetChanged();
-		}
+  private class NotifyingDataSetObserver extends DataSetObserver {
+    @Override
+    public void onChanged() {
+      super.onChanged();
+      dataValid = true;
+      notifyDataSetChanged();
+    }
 
-		@Override
-		public void onInvalidated() {
-			super.onInvalidated();
-			dataValid = false;
-			notifyDataSetChanged();
-		}
-	}
+    @Override
+    public void onInvalidated() {
+      super.onInvalidated();
+      dataValid = false;
+      notifyDataSetChanged();
+    }
+  }
 
 }
