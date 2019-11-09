523291224f5cdad1940ac2c837c80d5cf4fb9b4e
Dan Brough <dan@danbrough.org>
2019-11-10 10:27:07 +1300

    1.0.9

--- buildSrc/src/main/kotlin/ProjectVersions.kt
+++ buildSrc/src/main/kotlin/ProjectVersions.kt
@@ -3,7 +3,7 @@ import org.gradle.api.JavaVersion
 object ProjectVersions {
   const val SDK_VERSION = 29
   const val MIN_SDK_VERSION = 15
-  const val VERSION_CODE = 8
+  const val VERSION_CODE = 9
   val JAVA_VERSION = JavaVersion.VERSION_1_8
   val VERSION_NAME = getVersionName()
 
