0f087da41c9059f6f3de9f5fc74f171e1a6e2748
Dan Brough <dan@danbrough.org>
2019-11-17 10:07:19 +1300

    1.0.14-beta01

--- buildSrc/src/main/kotlin/ProjectVersions.kt
+++ buildSrc/src/main/kotlin/ProjectVersions.kt
@@ -3,13 +3,13 @@ import org.gradle.api.JavaVersion
 object ProjectVersions {
   const val SDK_VERSION = 29
   const val MIN_SDK_VERSION = 16
-  const val VERSION_CODE = 15
+  const val VERSION_CODE = 16
   val JAVA_VERSION = JavaVersion.VERSION_1_8
   val VERSION_NAME = getVersionName()
   const val GROUP_ID = "com.github.danbrough.androidutils"
   const val BETA_ID = 1
 
-  fun getVersionName(code: Int = VERSION_CODE) = "1.0.${code - 1}${if (BETA_ID > 0) "-beta%02d".format(
+  fun getVersionName(code: Int = VERSION_CODE) = "1.0.${code - 1- BETA_ID}${if (BETA_ID > 0) "-beta%02d".format(
     BETA_ID
   ) else ""}"
 }
