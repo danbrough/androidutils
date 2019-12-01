7a3cebd66affde949f6ff2d6d6e9a373f4163c3d
Dan Brough <dan@danbrough.org>
2019-12-02 08:46:36 +1300

    1.0.16

--- resource/src/main/java/danbroid/util/resource/ResourceUtils.kt
+++ resource/src/main/java/danbroid/util/resource/ResourceUtils.kt
@@ -3,6 +3,7 @@ package danbroid.util.resource
 import android.content.ContentResolver
 import android.content.Context
 import android.content.res.ColorStateList
+import android.content.res.TypedArray
 import android.net.Uri
 import android.util.TypedValue
 import android.widget.ImageView
@@ -58,18 +59,50 @@ object ResourceUtils {
    * Retrieves the [ColorInt] from your theme with the attribute identified by [themeColorAttribute]
    */
   @ColorInt
-  fun getThemeColour(context: Context, @AttrRes themeColorAttribute: Int): Int {
-    val typedValue = TypedValue()
-    val a = context.obtainStyledAttributes(typedValue.data, intArrayOf(themeColorAttribute))
-    val color = a.getColor(0, 0)
-    a.recycle()
-    return color
+  fun getThemeColour(context: Context, @AttrRes themeColorAttribute: Int, @ColorInt defValue: Int = 0): Int =
+    getAttribute(context, themeColorAttribute) {
+      it.getColor(0, defValue)
+    }
+
+  /**
+   * Retrieves the [Px] from your theme with the attribute identified by [dimensionAttribute]
+   */
+  @Px
+  fun getDimensionPixelSize(
+    context: Context, @AttrRes dimensionAttribute: Int,
+    defValue: Int = 0
+  ): Int = getAttribute(context, dimensionAttribute) {
+    it.getDimensionPixelSize(0, defValue)
   }
 
-  @ColorInt
-  fun getResourceColour(context: Context, @ColorRes colorId: Int): Int {
-    return ResourcesCompat.getColor(context.resources, colorId, context.theme)
+  /**
+   * Retrieves the [Px] from your theme with the attribute identified by [dimensionAttribute]
+   */
+  @Px
+  fun getDimension(
+    context: Context, @AttrRes dimensionAttribute: Int,
+    defValue: Float = 0f
+  ): Float =
+    getAttribute(context, dimensionAttribute) {
+      it.getDimension(0, defValue)
+    }
+
+  private fun <T> getAttribute(
+    context: Context, @AttrRes attribute: Int,
+    producer: (TypedArray) -> T
+  ): T {
+    val typedValue = TypedValue()
+    val a = context.obtainStyledAttributes(typedValue.data, intArrayOf(attribute))
+    return producer.invoke(a).also {
+      a.recycle()
+    }
   }
+
+
+  @ColorInt
+  fun getResourceColour(context: Context, @ColorRes colorId: Int): Int =
+    ResourcesCompat.getColor(context.resources, colorId, context.theme)
+
 }
 
 fun ImageView.setTint(@ColorRes tint: Int) =
@@ -101,6 +134,16 @@ fun Context.getThemeColour(@AttrRes themeColorAttribute: Int) =
 @ColorInt
 fun Context.getResourceColour(@ColorRes colorID: Int) =
   ResourceUtils.getResourceColour(this, colorID)
+
+
+@Px
+fun Context.getThemeDimension(@AttrRes themeDimensionAttr:Int, @Px defValue: Float = 0f) =
+  ResourceUtils.getDimension(this, themeDimensionAttr, defValue)
+
+@Px
+fun Context.getThemeDimensionPixelSize(@AttrRes themeDimensionAttr:Int, @Px defValue: Int = 0) =
+  ResourceUtils.getDimensionPixelSize(this, themeDimensionAttr, defValue)
+
 
 /**
  * calls [resolveStringURI] and removes any html tags
