e6dd87ba27aa3c4b92bf70b12953822c3ff40a9b
Dan Brough <dan@danbrough.org>
2021-11-16 09:44:55 +1300

    1.3.0

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Danbroid's Util Library
 
-### Latest version: 1.3.0-beta02
+### Latest version: 1.3.0
 
 ![alt text](https://jitpack.io/v/danbrough/androidutils.svg "Version Logo")
 
--- buildSrc/src/main/kotlin/ProjectVersions.kt
+++ buildSrc/src/main/kotlin/ProjectVersions.kt
@@ -1,4 +1,5 @@
 import org.gradle.api.JavaVersion
+import org.gradle.api.Project
 import java.util.*
 
 
@@ -13,17 +14,20 @@ object ProjectVersions {
   const val GROUP_ID = "com.github.danbrough.androidutils"
   const val KEYSTORE_PASSWORD = ""
   var VERSION_FORMAT = ""
-
+  lateinit var MAVEN_REPO: String
   val COMPOSE_VERSION = "1.1.0-beta02"
 
   val VERSION_NAME: String
     get() = getVersionName()
 
-  fun init(props: Properties) {
+  fun init(project: Project, props: Properties) {
     BUILD_VERSION = props.getProperty("buildVersion", "1").toInt()
     VERSION_OFFSET = props.getProperty("versionOffset", "1").toInt()
     VERSION_FORMAT = props.getProperty("versionFormat", "0.0.%d").trim()
+    MAVEN_REPO = project.findProperty("LOCAL_MAVEN_REPO")?.toString()?.trim()
+        ?: "https://h1.danbrough.org/maven"
   }
+
 
   fun getIncrementedVersionName() = getVersionName(BUILD_VERSION + 1)
 
